357e9858995608cab34e8b4791ec2c4f650d7aa3
Stylesmile <3239866994@qq.com>
2022-07-10 12:59:16 +0800

    fastboot-example

---
+++ fastboot-example/src/main/java/com/example/Application.java
@@ -0,0 +1,23 @@
+package com.example;
+
+
+import io.github.stylesmile.annotation.Controller;
+import io.github.stylesmile.annotation.RequestMapping;
+import io.github.stylesmile.app.App;
+
+/**
+ * 测试入口类
+ *
+ * @author stylesmile
+ */
+@Controller
+public class Application {
+    public static void main(String[] args) {
+        App.start(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "hello fastboot";
+    }
+}
---
+++ fastboot-example/src/main/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example;
+
+
+class DemoApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
