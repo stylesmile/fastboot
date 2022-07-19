3f96e7a4837b1dde4687c8160a733105fd301fab
Stylesmile <3239866994@qq.com>
2022-07-19 22:00:40 +0800

    优化ioc

--- fastboot-core/src/main/java/io/github/stylesmile/ioc/BeanFactory.java
+++ fastboot-core/src/main/java/io/github/stylesmile/ioc/BeanFactory.java
@@ -7,11 +7,13 @@ import io.github.stylesmile.tool.PropertyUtil;
 import io.github.stylesmile.tool.StringUtil;
 
 import javax.annotation.Resource;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Bean工厂
@@ -21,6 +23,20 @@ public class BeanFactory {
      * Bean容器
      */
     private static final Map<Class<?>, Object> classToBean = new ConcurrentHashMap<>();
+    private static final List<Class<? extends Annotation>> beanClasses = new CopyOnWriteArrayList<Class<? extends Annotation>>() {{
+        add(Controller.class);
+        add(Service.class);
+        add(Bean.class);
+    }};
+
+    /**
+     * 添加注册bea
+     *
+     * @param cls 注解名称
+     */
+    public void addBeanClasses(Class<? extends Annotation> cls) {
+        beanClasses.add(cls);
+    }
 
     /**
      * 获取一个Bean
@@ -60,17 +76,20 @@ public class BeanFactory {
 
     private static void putClassToBean(List<Class<?>> classList) {
         classList.forEach(cls -> {
-            boolean hasBeanAnno = cls.isAnnotationPresent(Bean.class);
-            boolean hasControllerAnno = cls.isAnnotationPresent(Controller.class);
-            boolean hasServiceSAnno = cls.isAnnotationPresent(Service.class);
-            if (hasBeanAnno || hasControllerAnno || hasServiceSAnno) {
+            boolean hasBean = cls.isAnnotationPresent(Bean.class);
+            for (Class beanClass : beanClasses) {
+                boolean b = cls.isAnnotationPresent(beanClass);
+                if (b) {
+                    hasBean = true;
+                    break;
+                }
+            }
+            if (hasBean) {
                 Object bean = null;
                 try {
                     bean = cls.newInstance();
                     classToBean.put(cls, bean);
-                } catch (InstantiationException e) {
-                    throw new RuntimeException(e);
-                } catch (IllegalAccessException e) {
+                } catch (InstantiationException | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
             }
@@ -81,42 +100,43 @@ public class BeanFactory {
      * 初始化Bean
      */
     private static boolean finishCreate(Class<?> cls) throws IllegalAccessException, InstantiationException {
-        boolean hasBeanAnno = cls.isAnnotationPresent(Bean.class);
-        boolean hasControllerAnno = cls.isAnnotationPresent(Controller.class);
-        boolean hasServiceSAnno = cls.isAnnotationPresent(Service.class);
-//        boolean hasMapSAnno = cls.isAnnotationPresent(Service.class);
-        //忽略，没有使用Bean注解和不是Controller、Service的类
-        if (!hasBeanAnno && !hasControllerAnno && !hasServiceSAnno) {
-            return true;
-        }
-
-        //创建Bean，处理对象中的属性，查看是否需要依赖注入
-        Object bean = cls.newInstance();
-        Field[] fields = cls.getDeclaredFields();
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(AutoWired.class) || field.isAnnotationPresent(Resource.class)) {
-                //获取属性的类型
-                Class<?> fieldType = field.getType();
-                //从工厂里面获取，获取不到，先返回
-                Object reliantBean = BeanFactory.getBean(fieldType);
-                if (reliantBean == null) {
-                    continue;
-                }
-                //从工厂获取到了，设置属性字段可接触
-                field.setAccessible(true);
-                //反射将对象设置到属性上
-                field.set(bean, reliantBean);
-                System.out.println();
-            } else if (field.getAnnotation(Value.class) != null) {
-                field.setAccessible(true);
-                Value value = field.getAnnotation(Value.class);
-                field.set(bean, StringUtil.isNotEmpty(value.value()) ? PropertyUtil.getProperty(value.value()) : null);
-                System.out.println("注入配置文件  " + bean.getClass() + " 加载配置属性" + value.value());
-                System.out.println(value);
+        boolean hasBean = cls.isAnnotationPresent(Bean.class);
+        for (Class beanClass : beanClasses) {
+            boolean b = cls.isAnnotationPresent(beanClass);
+            if (b) {
+                hasBean = true;
+                break;
             }
         }
-        //缓存实例到工厂中
-        classToBean.put(cls, bean);
+        if (hasBean) {
+            //创建Bean，处理对象中的属性，查看是否需要依赖注入
+            Object bean = cls.newInstance();
+            Field[] fields = cls.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(AutoWired.class) || field.isAnnotationPresent(Resource.class)) {
+                    //获取属性的类型
+                    Class<?> fieldType = field.getType();
+                    //从工厂里面获取，获取不到，先返回
+                    Object reliantBean = BeanFactory.getBean(fieldType);
+                    if (reliantBean == null) {
+                        continue;
+                    }
+                    //从工厂获取到了，设置属性字段可接触
+                    field.setAccessible(true);
+                    //反射将对象设置到属性上
+                    field.set(bean, reliantBean);
+                    System.out.println();
+                } else if (field.getAnnotation(Value.class) != null) {
+                    field.setAccessible(true);
+                    Value value = field.getAnnotation(Value.class);
+                    field.set(bean, StringUtil.isNotEmpty(value.value()) ? PropertyUtil.getProperty(value.value()) : null);
+                    System.out.println("注入配置文件  " + bean.getClass() + " 加载配置属性" + value.value());
+                    System.out.println(value);
+                }
+            }
+            //缓存实例到工厂中
+            classToBean.put(cls, bean);
+        }
         return true;
     }
 }
