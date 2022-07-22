ed060425abc9a2d7f4882c29ed5c6e1e7f0d0cd8
Stylesmile <3239866994@qq.com>
2022-07-22 17:25:13 +0800

    web 代码例子

--- fastboot-example/fastboot-web-example/src/main/java/com/example/Application.java
+++ fastboot-example/fastboot-web-example/src/main/java/com/example/Application.java
@@ -8,12 +8,16 @@ package com.example;
 import io.github.stylesmile.annotation.Controller;
 import io.github.stylesmile.annotation.RequestMapping;
 import io.github.stylesmile.app.App;
+import io.github.stylesmile.ioc.Value;
 import io.github.stylesmile.tool.PropertyUtil;
 
 @Controller
 public class Application {
     public Application() {
     }
+
+    @Value("fast.name")
+    String name;
 
     public static void main(String[] args) {
         App.start(Application.class, args);
@@ -25,7 +29,12 @@ public class Application {
     }
 
     @RequestMapping("/1")
-    public String hello2() {
+    public String hello1() {
         return PropertyUtil.props.getProperty("fast.name");
+    }
+
+    @RequestMapping("/2")
+    public String hello2() {
+        return name;
     }
 }
