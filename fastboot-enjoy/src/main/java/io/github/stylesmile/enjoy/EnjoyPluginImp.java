a2eb32d6863c468a660fe541e441fc17e74cbee4
Stylesmile <3239866994@qq.com>
2023-05-15 17:36:09 +0800

    fastboot-enjoy

---
+++ fastboot-enjoy/src/main/java/io/github/stylesmile/enjoy/EnjoyPluginImp.java
@@ -0,0 +1,90 @@
+package io.github.stylesmile.enjoy;
+
+import com.jfinal.template.Engine;
+import com.jfinal.template.ext.spring.JFinalViewResolver;
+import io.github.stylesmile.ioc.BeanContainer;
+import io.github.stylesmile.plugin.Plugin;
+
+/**
+ * @author chenye
+ * 2023.5.15
+ * @since 1.1.0
+ */
+public class EnjoyPluginImp implements Plugin {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void init() {
+        // 创建用于整合 spring boot 的 ViewResolver 扩展对象
+//        JFinalViewResolver jfr = new JFinalViewResolver();
+//        jfr.setBaseTemplatePath("");
+//        jfr.setEncoding("UTF-8");
+//        jfr.set
+//        jfr.setSuffix(".html");
+//        jfr.setContentType("text/html;charset=UTF-8");
+//        jfr.setOrder(0);
+
+        // 获取 engine 对象，对 enjoy 模板引擎进行配置，配置方式与前面章节完全一样
+        Engine engine  = JFinalViewResolver.engine;
+
+        // 热加载配置能对后续配置产生影响，需要放在最前面
+        engine.setDevMode(true);
+
+        // 使用 ClassPathSourceFactory 从 class path 与 jar 包中加载模板文件
+        engine.setToClassPathSourceFactory();
+
+        // 在使用 ClassPathSourceFactory 时要使用 setBaseTemplatePath
+        // 代替 jfr.setPrefix("/view/")
+        engine.setBaseTemplatePath("/view/");
+
+        // 添加模板函数
+//        engine.addSharedFunction("/common/_layout.html");
+//        engine.addSharedFunction("/common/_paginate.html");
+
+        // 更多配置与前面章节完全一样
+        // engine.addDirective(...)
+        // engine.addSharedMethod(...);
+
+        BeanContainer.setInstance(Engine.class, engine);
+//        BeanContainer.setInstance(JFinalViewResolver.class, jfr);
+    }
+
+    @Override
+    public void end() {
+
+    }
+//    @Override
+//    public void start(AopContext context) {
+//
+//        EnjoyRender render = EnjoyRender.global();
+//
+//        context.lifecycle(-99, () -> {
+//            context.beanForeach((k, v) -> {
+//                if (k.startsWith("view:")) { //java view widget
+//                    if (Directive.class.isAssignableFrom(v.clz())) {
+//                        render.putDirective(k.split(":")[1], (Class<? extends Directive>) v.clz());
+//                    }
+//                    return;
+//                }
+//
+//                if (k.startsWith("share:")) { //java share object
+//                    render.putVariable(k.split(":")[1], v.raw());
+//                    return;
+//                }
+//            });
+//        });
+//
+//        RenderManager.register(render);
+//        RenderManager.mapping(".shtm", render);
+
+//        if (ClassUtil.hasClass(() -> AuthUtil.class)) {
+//            render.putDirective(AuthConstants.TAG_authPermissions, AuthPermissionsTag.class);
+//            render.putDirective(AuthConstants.TAG_authRoles, AuthRolesTag.class);
+//        }
+//        BeanContainer.setInstance(SQLManager.class, sqlManager);
+//
+//    }
+}
---
+++ fastboot-enjoy/src/main/java/io/github/stylesmile/enjoy/EnjoyRender.java
@@ -0,0 +1,239 @@
+//package io.github.stylesmile.swagger;
+//
+//import com.jfinal.template.Directive;
+//import com.jfinal.template.Engine;
+//import com.jfinal.template.Template;
+//import com.jfinal.template.source.FileSourceFactory;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.File;
+//import java.io.OutputStream;
+//import java.io.PrintWriter;
+//import java.net.URI;
+//import java.net.URL;
+//
+///**
+// * Enjoy 视图渲染器
+// *
+// * @author noear
+// * @since 1.0
+// * */
+//public class EnjoyRender implements Render {
+//
+//    private static EnjoyRender _global;
+//
+//    public static EnjoyRender global() {
+//        if (_global == null) {
+//            _global = new EnjoyRender();
+//        }
+//
+//        return _global;
+//    }
+//
+//
+//    Engine provider = null;
+//    Engine provider_debug = null;
+//
+//    private ClassLoader classLoader;
+//
+//    //不要要入参，方便后面多视图混用
+//    //
+//    public EnjoyRender() {
+//        this(JarClassLoader.global());
+//    }
+//
+//    public EnjoyRender(ClassLoader classLoader) {
+//        this.classLoader = classLoader;
+//
+//        //开始中文支持
+//        Engine.setChineseExpression(true);
+//
+//        forDebug();
+//        forRelease();
+//
+//        Solon.app().shared().forEach((k, v) -> {
+//            putVariable(k, v);
+//        });
+//
+//        Solon.app().onSharedAdd((k, v) -> {
+//            putVariable(k, v);
+//        });
+//    }
+//
+//    public Engine getProvider() {
+//        return provider;
+//    }
+//
+//    public Engine getProviderDebug() {
+//        return provider_debug;
+//    }
+//
+//    private void forDebug() {
+//        if (Solon.cfg().isDebugMode() == false) {
+//            return;
+//        }
+//
+//        if (Solon.cfg().isFilesMode() == false) {
+//            return;
+//        }
+//
+//        if (provider_debug != null) {
+//            return;
+//        }
+//
+//        //添加调试模式
+//        URL rooturi = ResourceUtil.getResource("/");
+//        if (rooturi == null) {
+//            return;
+//        }
+//
+//        provider_debug = Engine.create("debug");
+//        provider_debug.setDevMode(true);
+//
+//
+//        String rootdir = rooturi.toString().replace("target/classes/", "");
+//        File dir = null;
+//
+//        if (rootdir.startsWith("file:")) {
+//            String dir_str = rootdir + "src/main/resources" + ViewConfig.getViewPrefix();
+//            dir = new File(URI.create(dir_str));
+//            if (!dir.exists()) {
+//                dir_str = rootdir + "src/main/webapp" + ViewConfig.getViewPrefix();
+//                dir = new File(URI.create(dir_str));
+//            }
+//        }
+//
+//        try {
+//            if (dir != null && dir.exists()) {
+//                provider_debug.setBaseTemplatePath(dir.getPath());
+//                provider_debug.setSourceFactory(new FileSourceFactory());
+//            }
+//
+//            //通过事件扩展
+//            EventBus.push(provider_debug);
+//        } catch (Exception e) {
+//            EventBus.pushTry(e);
+//        }
+//    }
+//
+//    private void forRelease() {
+//        if (provider != null) {
+//            return;
+//        }
+//
+//        provider = Engine.use();
+//        provider.setDevMode(Solon.cfg().isDebugMode());
+//
+//        try {
+//            provider.setBaseTemplatePath(ViewConfig.getViewPrefix());
+//            provider.setSourceFactory(new ClassPathSourceFactory2(classLoader));
+//
+//            //通过事件扩展
+//            EventBus.push(provider);
+//        } catch (Exception e) {
+//            EventBus.pushTry(e);
+//        }
+//    }
+//
+//    /**
+//     * 添加共享指令（自定义标签）
+//     */
+//    public void putDirective(String name, Class<? extends Directive> clz) {
+//        try {
+//            provider.addDirective(name, clz);
+//
+//            if (provider_debug != null) {
+//                provider_debug.addDirective(name, clz);
+//            }
+//        } catch (Exception e) {
+//            EventBus.pushTry(e);
+//        }
+//    }
+//
+//    /**
+//     * 添加共享变量
+//     */
+//    public void putVariable(String name, Object value) {
+//        try {
+//            provider.addSharedObject(name, value);
+//
+//            if (provider_debug != null) {
+//                provider_debug.addSharedObject(name, value);
+//            }
+//        } catch (Exception e) {
+//            EventBus.pushTry(e);
+//        }
+//    }
+//
+//    /**
+//     * 添加共享模板函数
+//     */
+//    public void putFunction(String path) {
+//        try {
+//            provider.addSharedFunction(path);
+//
+//            if (provider_debug != null) {
+//                provider_debug.addSharedFunction(path);
+//            }
+//        } catch (Exception e) {
+//            EventBus.pushTry(e);
+//        }
+//    }
+//
+//    @Override
+//    public void render(Object obj, Context ctx) throws Throwable {
+//        if (obj == null) {
+//            return;
+//        }
+//
+//        if (obj instanceof ModelAndView) {
+//            render_mav((ModelAndView) obj, ctx, () -> ctx.outputStream());
+//        } else {
+//            ctx.output(obj.toString());
+//        }
+//    }
+//
+//    @Override
+//    public String renderAndReturn(Object obj, Context ctx) throws Throwable {
+//        if (obj == null) {
+//            return null;
+//        }
+//
+//        if (obj instanceof ModelAndView) {
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            render_mav((ModelAndView) obj, ctx, () -> outputStream);
+//
+//            return outputStream.toString();
+//        } else {
+//            return obj.toString();
+//        }
+//    }
+//
+//    public void render_mav(ModelAndView mv, Context ctx, SupplierEx<OutputStream> outputStream) throws Throwable {
+//        if (ctx.contentTypeNew() == null) {
+//            ctx.contentType("text/html;charset=utf-8");
+//        }
+//
+//        if (ViewConfig.isOutputMeta()) {
+//            ctx.headerSet(ViewConfig.HEADER_VIEW_META, "EnjoyRender");
+//        }
+//
+//        Template template = null;
+//
+//        if (provider_debug != null) {
+//            try {
+//                template = provider_debug.getTemplate(mv.view());
+//            } catch (Exception e) {
+//                //忽略不计
+//            }
+//        }
+//
+//        if (template == null) {
+//            template = provider.getTemplate(mv.view());
+//        }
+//
+//        PrintWriter writer = new PrintWriter(outputStream.get());
+//        template.render(mv.model(), writer);
+//        writer.flush();
+//    }
+//}
