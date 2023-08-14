b26056fd3716877fb4e4a7f25e777e28e4172cd3
Stylesmile <3239866994@qq.com>
2023-08-14 21:22:43 +0800

    新增fastboot-jlhttpserver模块

---
+++ fastboot-server/fastboot-jlhttpserver/src/main/java/io/github/stylesmile/jedis/RedisConfig.java
@@ -0,0 +1,31 @@
+package io.github.stylesmile.jedis;
+
+import io.github.stylesmile.annotation.Service;
+import io.github.stylesmile.ioc.Bean;
+import io.github.stylesmile.tool.PropertyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisConfig {
+    public static Jedis getJedisPoolConfig() {
+        String host = PropertyUtil.getProperty("redis.host");
+        String port = PropertyUtil.getProperty("redis.port");
+        String db = PropertyUtil.getProperty("redis.db");
+        String password = PropertyUtil.getProperty("redis.password");
+        String timeout = PropertyUtil.getProperty("redis.timeout");
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+//        jedisPoolConfig.setMaxIdle(maxIdle);
+//        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        JedisPool jedisPool = new JedisPool(
+                jedisPoolConfig,
+                host,
+                Integer.parseInt(port),
+                Integer.parseInt(timeout),
+                password, db);
+        Jedis jedis = jedisPool.getResource();
+        return jedis;
+    }
+}
