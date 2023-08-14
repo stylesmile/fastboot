982fc0a1486f0f7f8dcab3a16e161cd1b470cdcd
Stylesmile <3239866994@qq.com>
2023-08-14 21:22:26 +0800

    新增fastboot-jdkhttpserver模块

---
+++ fastboot-server/fastboot-jdkhttpserver/src/main/java/io/github/stylesmile/mybatis/MybatisPlugin.java
@@ -0,0 +1,15 @@
+//package io.github.stylesmile.mybatis;
+//
+///**
+// * 参考文献 https://blog.csdn.net/qq_42413011/article/details/118640420
+// *
+// * @author Stylesmile
+// */
+//public class JdkHttpServerPlugin implements ServerPlugin {
+//
+//
+//
+//    public void init() {
+//
+//    }
+//}
