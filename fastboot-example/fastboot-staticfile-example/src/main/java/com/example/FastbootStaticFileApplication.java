b784a398effc6972df561cb7ca9c264ad0d04d1e
Stylesmile <3239866994@qq.com>
2024-02-13 12:26:01 +0800

    代码案例fastboot-staticfile-example

---
+++ fastboot-example/fastboot-staticfile-example/src/main/java/com/example/FastbootStaticFileApplication.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by Stylesmile
+// (powered by FernFlower decompiler)
+//
+
+package com.example;
+
+import io.github.stylesmile.annotation.Controller;
+import io.github.stylesmile.app.App;
+
+@Controller
+public class FastbootStaticFileApplication {
+    public static void main(String[] args) {
+        App.start(FastbootStaticFileApplication.class, args);
+    }
+}
