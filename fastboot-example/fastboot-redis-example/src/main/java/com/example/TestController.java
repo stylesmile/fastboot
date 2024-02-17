65c7874f93cf84121b316c249ac2a2fa5da00f9a
Stylesmile <3239866994@qq.com>
2024-02-17 18:41:50 +0800

    fastboot-redis-example

--- fastboot-example/fastboot-redis-example/src/main/java/com/example/TestController.java
+++ fastboot-example/fastboot-redis-example/src/main/java/com/example/TestController.java
@@ -4,6 +4,7 @@ import io.github.stylesmile.annotation.AutoWired;
 import io.github.stylesmile.annotation.Controller;
 import io.github.stylesmile.annotation.RequestMapping;
 import io.github.stylesmile.ioc.Value;
+import io.github.stylesmile.jedis.JedisTemplate;
 import redis.clients.jedis.Jedis;
 
 @Controller
@@ -11,6 +12,9 @@ public class TestController {
 
     @AutoWired
     Jedis jedis;
+
+    @AutoWired
+    JedisTemplate jedisTemplate;
 
     @Value(value = "fast.name")
     private String name;
@@ -23,7 +27,11 @@ public class TestController {
 
     @RequestMapping("/1")
     public String hello1() {
-        return jedis.set("test", "hello");
+        jedis.set("test1", "hello");
+        String s = jedis.get("test1");
+        jedisTemplate.set("test2", "hello");
+        String s2 = jedisTemplate.get("test2");
+        return s2;
     }
 
     @RequestMapping("/2")
