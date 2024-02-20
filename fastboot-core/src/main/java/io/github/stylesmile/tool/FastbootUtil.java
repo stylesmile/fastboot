87fe5011a687d45f2fbf50055e557fdfe41cc282
Stylesmile <3239866994@qq.com>
2024-02-20 20:22:51 +0800

    FastbootUtil.java

--- fastboot-core/src/main/java/io/github/stylesmile/tool/FastbootUtil.java
+++ fastboot-core/src/main/java/io/github/stylesmile/tool/FastbootUtil.java
@@ -1,12 +1,17 @@
 package io.github.stylesmile.tool;
 
 import io.github.stylesmile.app.App;
+import io.github.stylesmile.ioc.BeanContainer;
 
 /**
  * @author Stylesmile
  */
 public class FastbootUtil {
-    public static void addClass(Class clz){
+    public static void addClass(Class clz) {
         App.addClass(clz);
+    }
+
+    public static <T> T getBean(Class<T> clz) {
+        return BeanContainer.getInstance(clz);
     }
 }
