3b7bd70fa68dadbcfc34e60ee7b9c78236244d58
Stylesmile <3239866994@qq.com>
2024-02-20 16:53:42 +0800

    FastbootWebsocketApplication

---
+++ fastboot-example/fastboot-web-parent-example/fastboot-websocket-example/src/main/java/com/example/FastbootWebsocketApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.example.websocket.WebsocketStarter;
+import io.github.stylesmile.app.App;
+
+/**
+ * @author tanyaowu
+ */
+public class FastbootWebsocketApplication {
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+
+		//启动http server，这个步骤不是必须的，但是为了用页面演示websocket，所以先启动http
+		App.start(FastbootWebsocketApplication.class,args);
+		//启动websocket服务器
+		WebsocketStarter.start();
+	}
+}
