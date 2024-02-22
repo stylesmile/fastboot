08df554d983c9132549d962a7a7bd2c607af372d
Stylesmile <3239866994@qq.com>
2024-02-22 20:55:23 +0800

    StringUtil工具类优化

--- fastboot-core/src/main/java/io/github/stylesmile/tool/StringUtil.java
+++ fastboot-core/src/main/java/io/github/stylesmile/tool/StringUtil.java
@@ -5,13 +5,44 @@ package io.github.stylesmile.tool;
  * @author chenye
  */
 public class StringUtil {
-
+    /**
+     * 判断为空，不去除空格
+     *
+     * @param str 自促穿
+     * @return boolean
+     */
     public static boolean isEmpty(String str) {
         return (str == null || "".equals(str));
     }
 
+    /**
+     * 判断为非空，不去除空格
+     *
+     * @param str 自促穿
+     * @return boolean
+     */
     public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
+    }
+
+    /**
+     * 判断为非空空，去除 空格
+     *
+     * @param str 自促穿
+     * @return boolean
+     */
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
+    }
+
+    /**
+     * 判断为空，去除空格
+     *
+     * @param str 自促穿
+     * @return boolean
+     */
+    public static boolean isBlank(String str) {
+        return !isEmpty(str.trim());
     }
 
 }
