249047f9564030227ac80a4582220477f155df41
Stylesmile <3239866994@qq.com>
2024-02-24 21:18:27 +0800

    knife4j测试代码

--- fastboot-example/fastboot-web-parent-example/fastboot-knife4j-example/src/main/java/com/example/knife4j/FastbootKnife4jApplication.java
+++ fastboot-example/fastboot-web-parent-example/fastboot-knife4j-example/src/main/java/com/example/knife4j/FastbootKnife4jApplication.java
@@ -6,19 +6,22 @@ import io.github.stylesmile.annotation.RequestParam;
 import io.github.stylesmile.app.App;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @author chenye
  */
+@Tag(name = "test")
 @Controller
 public class FastbootKnife4jApplication {
     public static void main(String[] args) {
         App.start(FastbootKnife4jApplication.class, args);
     }
-    @Parameter(name = "name",description = "姓名",required = true)
+
+    @Parameter(name = "name", description = "姓名", required = true)
     @Operation(description = "向客人问好")
     @RequestMapping("/hello")
-    public String hello(@RequestParam(value = "name")String name){
-        return "Hi:"+name;
+    public String hello(@RequestParam(value = "name") String name) {
+        return "Hi:" + name;
     }
 }
