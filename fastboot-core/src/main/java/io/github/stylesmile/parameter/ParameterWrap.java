7b04db423596113c9580d2c16912fff32951e1e2
Stylesmile <3239866994@qq.com>
2024-02-24 16:38:05 +0800

    Parameter参数包装类

--- fastboot-core/src/main/java/io/github/stylesmile/parameter/ParameterWrap.java
+++ fastboot-core/src/main/java/io/github/stylesmile/parameter/ParameterWrap.java
@@ -3,8 +3,7 @@ package io.github.stylesmile.parameter;
 import io.github.stylesmile.tool.StringUtil;
 import lombok.Getter;
 
-import java.lang.reflect.*;
-import java.util.Map;
+import java.lang.reflect.Parameter;
 
 /**
  * Parameter参数包装类
@@ -12,44 +11,19 @@ import java.util.Map;
 @Getter
 public class ParameterWrap {
     private final Parameter parameter;
-    private Class<?> type;
-    private Type genericType;
     private String name;
 
     public ParameterWrap(Parameter parameter) {
-        this(parameter, null, null, null);
+        this(parameter, null);
     }
 
-    public ParameterWrap(Parameter parameter, Method method, Map<String, Type> genericInfo, String name) {
+    public ParameterWrap(Parameter parameter, String name) {
         super();
         this.parameter = parameter;
-        this.type = parameter.getType();
-        this.genericType = parameter.getParameterizedType();
         if (StringUtil.isNotBlank(name)) {
             this.name = name;
         } else {
             this.name = parameter.getName();
-        }
-
-        if (method != null) {
-
-            if (genericInfo != null && genericType instanceof TypeVariable) {
-                Type type0 = genericInfo.get(genericType.getTypeName());
-
-                if (type0 instanceof ParameterizedType) {
-                    genericType = type0;
-                    type0 = ((ParameterizedType) type0).getRawType();
-                }
-
-                if (type0 instanceof Class) {
-                    type = (Class<?>) type0;
-                } else {
-                    throw new IllegalStateException("Mapping mehtod generic analysis error: "
-                            + method.getDeclaringClass().getName()
-                            + "."
-                            + method.getName());
-                }
-            }
         }
     }
 
