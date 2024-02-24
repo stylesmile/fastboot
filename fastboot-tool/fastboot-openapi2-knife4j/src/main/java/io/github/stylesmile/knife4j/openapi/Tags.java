cb01d0a01da167586f80dec567f6e20dcaf82594
Stylesmile <3239866994@qq.com>
2024-02-24 20:36:22 +0800

    优化代码

---
+++ fastboot-tool/fastboot-openapi2-knife4j/src/main/java/io/github/stylesmile/knife4j/openapi/Tags.java
@@ -0,0 +1,13 @@
+package io.github.stylesmile.knife4j.openapi;
+
+import lombok.Data;
+
+@Data
+public class Tags {
+    private String name = "首页";
+    private String x_order = "123456";
+
+    public Tags(String tagStr) {
+        this.name = tagStr;
+    }
+}
