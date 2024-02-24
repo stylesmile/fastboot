b59c9554cacdc67024461705eb8bfc1b48077fc1
Stylesmile <3239866994@qq.com>
2024-02-24 20:51:40 +0800

    SwaggerInfo

--- fastboot-tool/fastboot-openapi2-knife4j/src/main/java/io/github/stylesmile/knife4j/openapi/SwaggerInfo.java
+++ fastboot-tool/fastboot-openapi2-knife4j/src/main/java/io/github/stylesmile/knife4j/openapi/SwaggerInfo.java
@@ -1,4 +1,4 @@
-package io.github.stylesmile.knife4j;
+package io.github.stylesmile.knife4j.openapi;
 
 import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
@@ -18,7 +18,7 @@ public class SwaggerInfo {
     private String host;
     private String basePath;
     private Tags tags;
-    private HashMap<String,Object> paths;
+    private HashMap<String, Object> paths;
     @SerializedName("x-openapi")
     private Xopenapi xopenapi;
 
@@ -30,39 +30,7 @@ public class SwaggerInfo {
         private String version = "2.0.0";
         private String title = "";
         private String termsOfService;
-
         private Contact contact;
         private License license;
-
-        @Data
-        @NoArgsConstructor
-        @AllArgsConstructor
-        private class Contact {
-            private String name;
-            private String url;
-            private String email;
-        }
-
-        @Data
-        private class License {
-            private String name = "test";
-            private String url = "https://stackoverflow.com/";
-        }
     }
-
-    @Data
-    private class Tags {
-        private String name = "首页";
-        private String x_order = "123456";
-    }
-
-    @Data
-    private class Xopenapi {
-        /**
-         * 接口信息
-         */
-        private String name;
-        private String x_order;
-    }
-
 }
