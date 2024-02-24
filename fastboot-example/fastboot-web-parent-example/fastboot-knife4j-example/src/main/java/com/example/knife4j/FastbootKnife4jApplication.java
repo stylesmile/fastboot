35d26ae8b593384204800b9aab48f97d25150a96
Stylesmile <3239866994@qq.com>
2024-02-24 21:06:03 +0800

    knife4j测试 代码

--- fastboot-example/fastboot-web-parent-example/fastboot-knife4j-example/src/main/java/com/example/knife4j/FastbootKnife4jApplication.java
+++ fastboot-example/fastboot-web-parent-example/fastboot-knife4j-example/src/main/java/com/example/knife4j/FastbootKnife4jApplication.java
@@ -1,12 +1,24 @@
 package com.example.knife4j;
 
+import io.github.stylesmile.annotation.Controller;
+import io.github.stylesmile.annotation.RequestMapping;
+import io.github.stylesmile.annotation.RequestParam;
 import io.github.stylesmile.app.App;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 /**
  * @author chenye
  */
+@Controller
 public class FastbootKnife4jApplication {
     public static void main(String[] args) {
         App.start(FastbootKnife4jApplication.class, args);
+    }
+    @Parameter(name = "name",description = "姓名",required = true)
+    @Operation(description = "向客人问好")
+    @RequestMapping("/sayHi2")
+    public String sayHi2(@RequestParam(value = "name")String name){
+        return "Hi:"+name;
     }
 }
