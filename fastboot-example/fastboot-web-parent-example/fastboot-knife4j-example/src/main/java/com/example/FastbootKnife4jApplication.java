143ff71d5fc8404aacc2af704fe941105120516d
Stylesmile <3239866994@qq.com>
2024-02-24 20:46:33 +0800

    FastbootKnife4jApplication

---
+++ fastboot-example/fastboot-web-parent-example/fastboot-knife4j-example/src/main/java/com/example/FastbootKnife4jApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import io.github.stylesmile.app.App;
+
+/**
+ * @author chenye
+ */
+public class FastbootKnife4jApplication {
+    public static void main(String[] args) throws Exception {
+
+        App.start(FastbootKnife4jApplication.class, args);
+    }
+}
