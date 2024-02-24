ca29352c4b43ef0cdccded68d6c06ce80ecc1f60
Stylesmile <3239866994@qq.com>
2024-02-24 21:34:29 +0800

    Tags

--- fastboot-tool/fastboot-openapi2-knife4j/src/main/java/io/github/stylesmile/knife4j/openapi/Tags.java
+++ fastboot-tool/fastboot-openapi2-knife4j/src/main/java/io/github/stylesmile/knife4j/openapi/Tags.java
@@ -7,7 +7,6 @@ public class Tags {
     private String name = "首页";
     private String x_order = "123456";
 
-    public Tags(String tagStr) {
-        this.name = tagStr;
+    public Tags() {
     }
 }
