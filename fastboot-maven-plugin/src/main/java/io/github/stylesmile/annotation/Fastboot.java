0fb3036b2997bcd914c4cc13c2fbb70b7e2075e1
Stylesmile <3239866994@qq.com>
2024-02-25 11:38:36 +0800

    Fastboot.java

---
+++ fastboot-maven-plugin/src/main/java/io/github/stylesmile/annotation/Fastboot.java
@@ -0,0 +1,9 @@
+package io.github.stylesmile.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Fastboot {
+}
