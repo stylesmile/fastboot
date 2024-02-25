b7995decbef42c8eb11d0f222e7c7e2d3674c08d
Stylesmile <3239866994@qq.com>
2024-02-25 11:46:32 +0800

    Fastboot-maven-plugin

---
+++ fastboot-maven-plugin/src/main/java/io/github/stylesmile/plugins/maven/CopyLoader.java
@@ -0,0 +1,105 @@
+package io.github.stylesmile.plugins.maven;
+
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.UUID;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+/**
+ * @author hxm
+ */
+public class CopyLoader {
+    static String path;
+    static String name;
+    static String tempName;
+    static String HServerName = "HServer.jar";
+
+    public static void start(File file) throws Exception {
+        CopyLoader.name = file.getName();
+        CopyLoader.tempName = UUID.randomUUID().toString() + ".jar";
+        CopyLoader.HServerName = UUID.randomUUID().toString() + ".jar";
+        CopyLoader.path = (file.getAbsolutePath().replace(name, ""));
+        getHServerJar();
+        setLoader();
+        organizeFiles();
+    }
+
+
+    private static void organizeFiles() {
+        new File(path + name).delete();
+        new File(path + HServerName).delete();
+        new File(path + tempName).renameTo(new File(path + name));
+
+    }
+
+    private static void setLoader() throws IOException {
+        JarFile jarfile = new JarFile(path + HServerName);
+        JarFile targetJarfile = new JarFile(path + name);
+        JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(path + tempName));
+        //写loader 文件
+        Enumeration<JarEntry> entries = jarfile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            if (!jarEntry.isDirectory() && jarEntry.getName().contains("top/hserver/core/loader/") && jarEntry.getName().endsWith(".class")) {
+                jarOutputStream.putNextEntry(jarEntry);
+                byte[] bytes = toByteArray(jarfile.getInputStream(jarEntry));
+                jarOutputStream.write(bytes);
+            }
+        }
+        //写原文件
+        Enumeration<JarEntry> targetEntries = targetJarfile.entries();
+        while (targetEntries.hasMoreElements()) {
+            JarEntry entry = targetEntries.nextElement();
+            InputStream entryInputStream = targetJarfile
+                    .getInputStream(entry);
+            jarOutputStream.putNextEntry(entry);
+            jarOutputStream.write(toByteArray(entryInputStream));
+        }
+        jarOutputStream.flush();
+        jarOutputStream.close();
+        targetJarfile.close();
+        jarfile.close();
+    }
+
+    private static void getHServerJar() throws Exception {
+        JarFile jarfile = new JarFile(path + name);
+        Enumeration<JarEntry> entries = jarfile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            if (jarEntry.getName().endsWith(".jar") && jarEntry.getName().startsWith("lib/HServer")) {
+                InputStream inputStream = jarfile.getInputStream(jarEntry);
+                writeToLocal(path + HServerName, inputStream);
+                break;
+            }
+        }
+        jarfile.close();
+
+    }
+
+    private static byte[] toByteArray(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+        }
+        return output.toByteArray();
+    }
+
+    private static void writeToLocal(String destination, InputStream input)
+            throws IOException {
+        int index;
+        byte[] bytes = new byte[1024];
+        FileOutputStream downloadFile = new FileOutputStream(destination);
+        while ((index = input.read(bytes)) != -1) {
+            downloadFile.write(bytes, 0, index);
+            downloadFile.flush();
+        }
+        downloadFile.close();
+        input.close();
+    }
+
+}
