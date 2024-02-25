71c3d7eca3f93baee1d218daca469def942868b6
Stylesmile <3239866994@qq.com>
2024-02-25 11:48:41 +0800

    Fastboot-maven-plugin

---
+++ fastboot-maven-plugin/src/main/java/io/github/stylesmile/plugins/maven/tools/Constant.java
@@ -0,0 +1,22 @@
+package io.github.stylesmile.plugins.maven.tools;
+
+
+/**
+ * @author hxm
+ */
+public class Constant {
+
+    public static final String MDW_PATH       = "/opt/hserver/mdw";
+
+    public static final String JAR_TOOL       = "HServer-Jar-Build-Tool";
+
+    public static final String JAR_TOOL_VALUE = "HServer-Jar-Plugin";
+
+    public static final String START_CLASS = "Start-Class";
+
+    public static final String HEAD_PACKAGE = "top.hserver.core.loader";
+
+    public static final String MAIN_CLASS = "top.hserver.core.loader.HServerJarMainClassStart";
+
+
+}
