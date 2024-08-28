f60e9665b5c5c385be5646df37e24dc252f22ca5
Stylesmile <3239866994@qq.com>
2024-08-28 18:07:23 +0800

    fastboot-web-mqtt-server-example优化

--- fastboot-example/fastboot-web-mqtt-server-example/src/main/java/com/example/web/MqttServerApplication.java
+++ fastboot-example/fastboot-web-mqtt-server-example/src/main/java/com/example/web/MqttServerApplication.java
@@ -19,76 +19,16 @@ import java.util.Map;
 /**
  * 启动时，只需扫描 controller service 和配置，这样可以提高启动速度
  */
-@Fastboot(exclude = {User.class})
+@Fastboot
 @Controller
 public class  MqttServerApplication {
 
-    @Value("fastboot.name")
-    String name;
 
     public static void main(String[] args) {
         for (int a = 0; a < args.length; a++) {
             System.out.println(args[a]);
         }
         App.start(MqttServerApplication.class, args);
-    }
-
-    @RequestMapping("/")
-    public String hello() {
-        return "hello fastboot";
-    }
-
-    @RequestMapping("/1")
-    public String hello1() {
-        return PropertyUtil.props.getProperty("fast.name");
-    }
-
-    @RequestMapping("/2")
-    public String hello2() {
-        return name;
-    }
-
-    @RequestMapping("/3")
-    public String hello3(@RequestParam("name") String name) {
-        return name;
-    }
-
-    @RequestMapping("/4")
-    public Map<String, String> hello4() {
-        Map<String, String> map = new HashMap<>();
-        map.put("1", "1");
-        map.put("2", "1");
-        System.out.println(JsonGsonUtil.objectToJson(map));
-        return map;
-    }
-
-    @RequestMapping("/5")
-    public User hello5() {
-        User user = new User();
-        user.setName("lisi");
-        user.setAge(18);
-        System.out.println(JsonGsonUtil.objectToJson(user));
-        return user;
-    }
-
-    @RequestMapping("/6")
-    public String test(UploadedFile file, String username) throws IOException {
-        file.save("d://test//" + System.currentTimeMillis() + file.getName());
-        return "success~" + username;
-    }
-
-    @RequestMapping("/7")
-    public String test(String password, String username) throws IOException {
-        return "username~" + username + "， password: " + password;
-    }
-
-    @RequestMapping("/8")
-    public String test8(@RequestBody User user) throws IOException {
-        return "username~" + user.getName() + "， password: " + user.getName();
-    }
-    @RequestMapping("/9")
-    public String test8(@RequestParam("username1") String username) throws IOException {
-        return "username~" + username ;
     }
 
 }
