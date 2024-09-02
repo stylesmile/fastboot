916c320e451fd80e8153496e901900d2340754c1
Stylesmile <3239866994@qq.com>
2024-09-02 17:21:37 +0800

    升级到fastcloud 0.3.0

--- fast-cloud-parent/fast-cloud-minio/src/main/java/io/github/stylesmile/minio/MinioConfig.java
+++ fast-cloud-parent/fast-cloud-minio/src/main/java/io/github/stylesmile/minio/MinioConfig.java
@@ -6,16 +6,22 @@ import io.minio.MinioClient;
 public class MinioConfig {
     /**
      * 获取 minio 客户端
+     *
      * @return MinioClient
      */
     public static MinioClient getMinioClient() {
-        String endpoint = PropertyUtil.getProperty("minio.endpoint");
-        String accessKey = PropertyUtil.getProperty("minio.accessKey");
-        String secretKey = PropertyUtil.getProperty("minio.secretKey");
-        MinioClient minioClient = MinioClient.builder()
-                .endpoint(endpoint)
-                .credentials(accessKey, secretKey)
-                .build();
+        MinioClient minioClient = null;
+        try {
+            String endpoint = PropertyUtil.getProperty("minio.endpoint");
+            String accessKey = PropertyUtil.getProperty("minio.accessKey");
+            String secretKey = PropertyUtil.getProperty("minio.secretKey");
+            minioClient = MinioClient.builder()
+                    .endpoint(endpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+        } catch (Exception e) {
+            System.err.println("minio 配置错误" + e.getMessage());
+        }
         return minioClient;
     }
 }
