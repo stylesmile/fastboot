9040db8738b0b1ec80008eb2602e26d95a0347fa
xiaofeng <xiaofeng@169.com>
2026-09-17 16:45:23 +0800

    fastboot-flyway-example

---
+++ fastboot-example/fastboot-tool-example-parent/fastboot-flyway-example/src/main/java/com/example/FastbootFlywayApplication.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import io.github.stylesmile.app.App;
+
+public class FastbootFlywayApplication {
+    public static void main(String[] args) {
+        App.start(FastbootFlywayApplication.class, args);
+    }
+}
